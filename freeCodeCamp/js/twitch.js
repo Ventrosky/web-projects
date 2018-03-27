a9dfc274460b504aff34ea3837d450b758fb11bc
Salvatore Ventrone <ventrosky@gmail.com>
2018-03-27 22:40:03 +0200

    Update twitch.js

--- freeCodeCamp/js/twitch.js
+++ freeCodeCamp/js/twitch.js
@@ -3,13 +3,12 @@ var streamList = ["buccaneerdev", "burkeblack", "ESL_SC2", "freecodecamp", "Ogam
 
 $(document).ready(function() {
 	function createResult(name, logo, stream, status, link){
+		name = streamList[streamList.findIndex(item => item.toLowerCase() == name)];
 		var htmlButton = (stream != 'offline' ? ('<a href="'+link+'" target="_blank" class="btn btn-success tooltips" data-placement="top" data-toggle="tooltip" data-original-title="Online"><i class="fa fa-twitch"></i></a>') : ('<a href="'+link+'" target="_blank" class="btn btn-danger tooltips" data-placement="top" data-toggle="tooltip" data-original-title="Offline"><i class="fa fa-twitch"></i></a>'));
 		var htmlCode = '<div class="col-sm-6 col-centered"><div class="panel"><div class="panel-body p-t-10"><div class="media-main"><a class="pull-left" href="#"><img class="thumb-lg img-circle bx-s" src="'+logo+'" alt=""></a><div class="pull-right btn-group-sm">'+htmlButton+'</div><div class="info"><h4>'+name+'</h4><p class="text-muted">'+status+'</p></div></div></div></div></div><div class="clearfix"></div>';
 		$("#channels").append($.parseHTML(htmlCode));
-    
 		$(function(){$("a[data-toggle='tooltip']").tooltip();});
 	 }  
-	
 	for (i=0; i< streamList.length;i++){
 		$.getJSON(baseURL+'/streams/'+streamList[i]+'?callback=?', function(data) {
   			if (data.stream != null){
@@ -20,5 +19,4 @@ $(document).ready(function() {
     			}
 		});
 	};
-
 });
