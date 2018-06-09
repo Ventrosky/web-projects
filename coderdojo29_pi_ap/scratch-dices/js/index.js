19f835f907a07364a59ca897d66c43e5e52639ea
Salvatore Ventrone <ventrosky@gmail.com>
2018-06-09 10:17:50 +0200

    Add files via upload

--- coderdojo29_pi_ap/scratch-dices/js/index.js
+++ coderdojo29_pi_ap/scratch-dices/js/index.js
@@ -1,8 +1,9 @@
-var results = { chance: 3, dices: []}
-const baseURL = "https://raw.githubusercontent.com/Ventrosky/web-projects/master/coderdojo29_pi_ap/img/dadi/";
+const MAX = 3;
+var results = { chance: MAX, dices: []}
+const baseURL = "https://raw.githubusercontent.com/PisaCoderDojo/Scratch-lessons/master/scratch-dices/img/dadiv2/";
 
 function loadResults(){
-    let placeHolder = [3,"dado-0.png","dado-0.png","dado-0.png","dado-0.png","dado-0.png","dado-0.png"];
+    let placeHolder = [MAX,"dado-0.png","dado-0.png","dado-0.png","dado-0.png","dado-0.png","dado-0.png"];
     let itemsArray = localStorage.getItem('_BuccaneerDev_dices') ? JSON.parse(localStorage.getItem('_BuccaneerDev_dices')) : placeHolder;
     results = {dices: itemsArray.slice(-1,1), chance: parseInt(itemsArray[0])};
     let i =1;
@@ -26,17 +27,24 @@ $(function() {
  loadResults();
  $('.random').on("click",function(){
    if (results.chance <= 0) return null;
-   var die = 1;
-   results.chance = results.chance -1;
-   $( "#chance" ).text(results.chance);
-   results.dices = [];
-   $('td:first-child').each(function() {
+   let r = true;
+   if(results.chance < MAX){
+     let txt = "Sicuro di voler rilanciare i dadi? Hai solo "+(results.chance == 1 ? "un altro tentativo!" : "altri "+results.chance+" tentativi!" );
+     r = confirm(txt);
+   }
+   if (r == true) {
+    var die = 1;
+    results.chance = results.chance -1;
+    $( "#chance" ).text(results.chance);
+    results.dices = [];
+    $('td:first-child').each(function() {
         let rndX = rngDice();
         let dieName = "dado-"+die+"-"+rndX+".png";
         results.dices.push(dieName);
         $(this).children(":first").attr('src',baseURL+dieName);
         die += 1;
-    });
-    saveResults();
+      });
+      saveResults();
+    }
  });
 });
