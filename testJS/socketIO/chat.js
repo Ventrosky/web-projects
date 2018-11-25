716fc046277efc84ed3e578e286012024cb9e19f
Salvatore Ventrone <ventrosky@gmail.com>
2018-11-25 14:09:06 +0100

    express
    .emit, .on, .connect

---
+++ testJS/socketIO/chat.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const socketio = require('socket.io');
+
+app.use(express.static(__dirname + '/public'));
+
+const expressServer = app.listen(9009);
+
+const io = socketio(expressServer);
+
+io.on('connection', socket => {
+    socket.emit('messageFromServer', {data: "Welcome to socket.io Server"});
+    socket.on('messageToServer', dataFC => {
+        console.log(dataFC);
+    })
+})
