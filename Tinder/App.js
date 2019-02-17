41fe95bd1be94f8012450cbec416cef5db7f8a6c
Salvatore Ventrone <ventrosky@gmail.com>
2019-02-17 23:29:42 +0100

    tinder clone
    basic react native

---
+++ Tinder/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './styles.js'
+import RootNavigator from './navigation/RootNavigator.js'
+
+import { 
+    Text, 
+    View 
+} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <RootNavigator/>
+    );
+    //<View style={styles.container}> </View>
+  }
+}
+
---
+++ Tinder/navigation/TabNavigator.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Home from '../screens/Home';
+import Profile from '../screens/Profile';
+import Matches from '../screens/Matches';
+import { createMaterialTopTabNavigator } from 'react-navigation';
+
+export default createMaterialTopTabNavigator(
+  {
+    Profile: {
+      screen: Profile,
+      navigationOptions: {
+        tabBarLabel: 'Profile',
+      },
+    },
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        tabBarLabel: 'Home',
+      }
+    },
+
+    Matches: {
+      screen: Matches,
+      navigationOptions: {
+        tabBarLabel: 'Matches',
+      },
+    },
+  },
+  {
+    navigationOptions: {
+      header: null
+    },
+    tabBarPosition: 'top',
+    initialRouteName: 'Home',
+    animationEnabled: true,
+    swipeEnabled: false,
+    tabBarOptions: {
+      upperCaseLabel: false,
+      style: {
+        height: 60,
+        backgroundColor: '#555',
+      },
+    }
+  }
+);
