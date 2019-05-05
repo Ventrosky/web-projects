d08880e115b898d6f035f79c82e37c31daa9b904
Salvatore Ventrone <ventrosky@gmail.com>
2019-05-05 13:59:55 +0200

    Pug Template
    Templating Engines - Pug

--- testJS/express/app.js
+++ testJS/express/app.js
@@ -5,15 +5,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+app.set('view engine', 'pug');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 // parser
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 //app.use(bodyParser.json());
@@ -24,7 +26,8 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
     //res.status(404).send('<h1>Page Not Found</h1>');
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    //res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 
--- testJS/express/routes/shop.js
+++ testJS/express/routes/shop.js
@@ -3,12 +3,16 @@ const rootDir = require('../util/path');
 const express = require('express');
 
 const router = express.Router()
+const adminData = require('./admin');
 
 // get do exact match, unlike use
 router.get('/',(req, res, next) => {
-    console.log("This is another middleware");
+    console.log(adminData.products);
     //res.send('<h1>Hello</h1>');
-    res.sendFile(path.join(rootDir,'views','shop.html'));
+    //res.sendFile(path.join(rootDir,'views','shop.html'));
+
+    const products = adminData.products;
+    res.render('shop',{prods: products, pageTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;
