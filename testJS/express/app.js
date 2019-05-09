7b89faaa8fd4a26a5e025893a9fa2f70a0ed7717
Salvatore Ventrone <ventrosky@gmail.com>
2019-05-09 09:10:42 +0200

    EJS
    EJS -- Embedded JavaScript templates

--- testJS/express/app.js
+++ testJS/express/app.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set('view engine', 'pug');
+//app.set('view engine', 'pug'); //for pug 
+app.set('view engine', 'ejs');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
--- testJS/express/routes/admin.js
+++ testJS/express/routes/admin.js
@@ -10,7 +10,13 @@ router.get('/add-product', (req, res, next) => {
     //res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
     //res.sendFile(path.join(rootDir,'views','add-product.html'));/*same as
     //res.sendFile(path.join(__dirname,'..','views','add-product.html'));*/
-    res.render('add-product',{pageTitle: 'Add Products', path:'/admin/add-product'});
+    res.render('add-product', {
+		pageTitle: 'Add Product',
+		path: '/admin/add-product',
+		formsCSS: true,
+		productCSS: true,
+		activeAddProduct: true
+	});
   });
   
   router.post('/add-product', (req, res, next) => {
